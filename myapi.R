133093ebd7b07dbeac36dddc3719cf42a897c852
Tasos Psychogyiopoulos <78209101+tasospsy@users.noreply.github.com>
2025-10-22 17:12:14 +0300

    Create myapi.R

---
+++ myapi.R
@@ -0,0 +1,38 @@
+# Load libraries
+library(plumber)
+library(jsonlite)
+
+#* @filter cors
+function(req, res) {
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  res$setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+  res$setHeader("Access-Control-Allow-Headers", "Content-Type, x-api-key")
+  
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$status <- 200
+    return(list())  # preflight response
+  }
+  
+  forward()
+}
+
+
+#* @filter check_api_key
+function(req, res) {
+  token <- req$HTTP_X_API_KEY
+  if (is.null(token) || token != "MmO1HH7Oeer6iZjgNsLsjENOmXWTJtmq2oPJXhla") {
+    res$status <- 401
+    return(list(error = "Unauthorized"))
+  }
+  forward()
+}
+
+#* @post /score
+function(req, res) {
+  body <- jsonlite::fromJSON(req$postBody)
+  # Debug: print what Qualtrics sent
+  print(body) 
+  
+  score <- as.numeric(body$answer1) + as.numeric(body$answer2)
+  list(score = score)
+}
