20b97c00a8acf83e9cd63212acfb3dd822459ba8
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2025-10-24 11:36:50 +0300

    add questions

--- myapi.R
+++ myapi.R
@@ -27,9 +27,25 @@ function(req, res) {
   forward()
 }
 # END SETTINGS ------------------------------------------------------------
+
+# CAT specs ---------------------------------------------------------------
+# Make Question and Choices characters
+questions <- c(
+  "Little interest or pleasure in doing things",
+  "Feeling down, depressed, or hopeless",
+  "Trouble falling or staying asleep, or sleeping too much",
+  "Feeling tired or having little energy",
+  "Poor appetite or overeating",
+  "Feeling bad about yourself or that you are a failure or  have let yourself or your family down",
+  "Trouble concentrating on things, such as reading the  newspaper or watching television",
+  "Moving or speaking so slowly that other people could have noticed.  Or the opposite    being so figety or restless that you have been  moving around a lot more than usual",
+  "Thoughts that you would be better off dead, or of hurting yourself"
+)
+
 ## Env variables
 myenv <- new.env()
 myenv$ud_se <- NULL # user defined standard error
+myenv$qs <- questions # user defined standard error
 
 #* @post /test
 function(req, res) {
@@ -37,7 +53,11 @@ function(req, res) {
   # Debug: print what Qualtrics sent
   #print(body) 
   se <- as.numeric(body$slider) 
-  message("Iter is: ", as.numeric(body$iter))
+  message("\nIter is: ", as.numeric(body$iter))
+  
+  next_q_number <- sample(1:9,1)
+  next_q <- myenv$qs[next_q_number]
   myenv$ud_se <- se
-  list(se = se)
+  
+  list(se = se, next_q = next_q)
 }
