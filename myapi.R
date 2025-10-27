e9ab58088ed33d45cab7e98597557bafc28052cc
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2025-10-27 11:56:14 +0200

    complete irt cat 1st attempt

--- myapi.R
+++ myapi.R
@@ -29,24 +29,15 @@ function(req, res) {
 # END SETTINGS ------------------------------------------------------------
 
 # CAT specs ---------------------------------------------------------------
-# Make Question and Choices characters
-questions <- c(
-  "Little interest or pleasure in doing things",
-  "Feeling down, depressed, or hopeless",
-  "Trouble falling or staying asleep, or sleeping too much",
-  "Feeling tired or having little energy",
-  "Poor appetite or overeating",
-  "Feeling bad about yourself or that you are a failure or  have let yourself or your family down",
-  "Trouble concentrating on things, such as reading the  newspaper or watching television",
-  "Moving or speaking so slowly that other people could have noticed.  Or the opposite    being so figety or restless that you have been  moving around a lot more than usual",
-  "Thoughts that you would be better off dead, or of hurting yourself"
-)
+## READ CAT SPECS
+link <- "https://github.com/tasospsy/q-cats/raw/refs/heads/main/calibrations/phq9-irt-cal-results.RData"
+load(url(link))
 J <- length(questions)
 ## Env variables
 myenv <- new.env()
 myenv$ud_se <- NULL # user defined standard error
 myenv$qs <- questions # user defined standard error
-
+myenv$mod <- mod
 
 #* @post /test
 function(req, res) {
