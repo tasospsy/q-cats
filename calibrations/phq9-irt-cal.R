bfbc9cd96c4805f9a85a4339299f3e278c030565
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2025-10-28 09:14:22 +0200

    update qs

--- calibrations/phq9-irt-cal.R
+++ calibrations/phq9-irt-cal.R
@@ -2,15 +2,15 @@
 # phq9 cat settings -------------------------------------------------------
 # Make Question and Choices characters
 questions <- c(
-  "Little interest or pleasure in doing things",
-  "Feeling down, depressed, or hopeless",
-  "Trouble falling or staying asleep, or sleeping too much",
-  "Feeling tired or having little energy",
-  "Poor appetite or overeating",
-  "Feeling bad about yourself or that you are a failure or  have let yourself or your family down",
-  "Trouble concentrating on things, such as reading the  newspaper or watching television",
-  "Moving or speaking so slowly that other people could have noticed.  Or the opposite    being so figety or restless that you have been  moving around a lot more than usual",
-  "Thoughts that you would be better off dead, or of hurting yourself"
+  "Over the last 2 weeks, how often have you been bothered by little interest or pleasure in doing things",
+  "Over the last 2 weeks, how often have you been bothered by feeling down, depressed, or hopeless",
+  "Over the last 2 weeks, how often have you been bothered by trouble falling or staying asleep, or sleeping too much",
+  "Over the last 2 weeks, how often have you been bothered by feeling tired or having little energy",
+  "Over the last 2 weeks, how often have you been bothered by poor appetite or overeating",
+  "Over the last 2 weeks, how often have you been bothered by feeling bad about yourself or that you are a failure or  have let yourself or your family down",
+  "Over the last 2 weeks, how often have you been bothered by trouble concentrating on things, such as reading the  newspaper or watching television",
+  "Over the last 2 weeks, how often have you been bothered by moving or speaking so slowly that other people could have noticed.  Or the opposite    being so figety or restless that you have been  moving around a lot more than usual",
+  "Over the last 2 weeks, how often have you been bothered by thoughts that you would be better off dead, or of hurting yourself"
 )
 
 ## Define answer options
