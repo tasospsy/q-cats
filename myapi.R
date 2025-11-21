d634b13fe8c7119fe6e0e92f1be250de0d5d21ec
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2025-11-21 11:39:22 +0200

    split to cat api and no-cat api

--- myapi.R
+++ myapi.R
@@ -21,6 +21,8 @@ function(req, res) {
 # if the key is inside th elist
 # for now:
 KEY <- "test"
+##
+catName <- "PHQ9" #@todo
 
 #* @filter check_api_key
 function(req, res) {
@@ -37,11 +39,20 @@ function(req, res) {
 library(mirt)
 library(mirtCAT)
 ## READ CAT SPECS
-load("uploads/irt-calib-results.RData")
-df <- read.csv("uploads/items_df.csv")
-J <- nrow(df)
+if(file.exists("uploads/irt-calib-results.RData")){
+  load("uploads/irt-calib-results.RData")
+  cat("IRT Calibration file is loaded\n")
+} else {
+  cat("IRT Calibration file is not uploaded\n")
+}
 
-catName <- "PHQ9" #@todo
+if (file.exists("uploads/items_df.csv")) {
+  df <- read.csv("uploads/items_df.csv")
+  J <- nrow(df)
+  cat("CSV file is loaded\n")
+} else {
+  cat("CSV file is not uploaded\n")
+}
 
 ## Global settings
 sessions <- new.env(parent = emptyenv())
@@ -182,80 +193,3 @@ function(req, res) {
       )
     )} 
 } 
-
-#* @get /user-results
-function(req, res) {
-  api_key <- req$HTTP_X_API_KEY
-  if (is.null(api_key) || api_key != KEY) {
-    res$status <- 401
-    return(list(error = "Invalid or missing API key"))
-  }
-  userid <- req$HTTP_USERID
-  if (is.null(userid) || userid == "") {
-    res$status <- 400
-    return(list(error = "Missing userid header"))
-  }
-  
-  patt <- paste0("^", userid, "_.*_session\\.json$")
-  file <- list.files("sessions", pattern = patt, full.names = TRUE)
-  if (length(file) == 0) {
-    res$status <- 404
-    return(list(error = paste("No session file found for user:", userid)))
-  }
-  if (length(file) > 1) {
-    res$status <- 404
-    return(list(error = paste("Multiple sessions for user:", userid)))
-  }
-  jsonlite::fromJSON(file) #just return it as list
-}
-
-#* @get /get-data
-#* @serializer csv
-function(req, res) {
-  api_key <- req$HTTP_X_API_KEY
-  if (is.null(api_key) || api_key != KEY) {
-    res$status <- 401
-    return(list(error = "Invalid or missing API key"))
-  }
-  
-  files <- list.files("sessions", full.names = TRUE)
-  tmp <- list()
-  for(f in 1:length(files)) tmp[[f]] <- jsonlite::fromJSON(files[f])
-  res.df <- as.data.frame(do.call(rbind, tmp))
-  # list-columns to comma-separated strings
-  res.df[] <- lapply(res.df, function(col) {
-    if (is.list(col)) sapply(col, toString) else col
-  })
-  
-  res$setHeader(
-    "Content-Disposition",
-    paste0('attachment; filename="session_data_', format(Sys.time(), "%Y-%m-%d_%H-%M"), '.csv"')
-  )
-  return(res.df)
-}
-
-#* @post /upload
-#* @parser multi
-function(req, res) {
-  
-  api_key <- req$HTTP_X_API_KEY
-  if (is.null(api_key) || api_key != KEY) {
-    res$status <- 401
-    return(list(error = "Invalid or missing API key"))
-  }
-  upload_dir <- "uploads"
-  if (!dir.exists(upload_dir)) 
-    dir.create(upload_dir, showWarnings = FALSE)
-  
-  for(i in 1:length(req$body)){
-    dest <- file.path("uploads", req$body[[i]]$filename)
-    writeBin(req$body[[i]]$value, dest)  
-  }
-
-  list(
-    status = "ok",
-    saved_as = basename(dest)
-  )
-}
-
-
---
+++ otherapi.R
@@ -0,0 +1,80 @@
+# -------------------------------------------------------------------------
+# HELPER FUNCTIONS API ----------------------------------------------------
+# -------------------------------------------------------------------------
+#* USER RESULTS TO JSON 
+#* @get /user-results
+function(req, res) {
+  api_key <- req$HTTP_X_API_KEY
+  if (is.null(api_key) || api_key != KEY) {
+    res$status <- 401
+    return(list(error = "Invalid or missing API key"))
+  }
+  userid <- req$HTTP_USERID
+  if (is.null(userid) || userid == "") {
+    res$status <- 400
+    return(list(error = "Missing userid header"))
+  }
+  
+  patt <- paste0("^", userid, "_.*_session\\.json$")
+  file <- list.files("sessions", pattern = patt, full.names = TRUE)
+  if (length(file) == 0) {
+    res$status <- 404
+    return(list(error = paste("No session file found for user:", userid)))
+  }
+  if (length(file) > 1) {
+    res$status <- 404
+    return(list(error = paste("Multiple sessions for user:", userid)))
+  }
+  jsonlite::fromJSON(file) #just return it as list
+}
+
+#* GET RESULTS IN CSV TABLE
+#* @get /get-data
+#* @serializer csv
+function(req, res) {
+  api_key <- req$HTTP_X_API_KEY
+  if (is.null(api_key) || api_key != KEY) {
+    res$status <- 401
+    return(list(error = "Invalid or missing API key"))
+  }
+  
+  files <- list.files("sessions", full.names = TRUE)
+  tmp <- list()
+  for(f in 1:length(files)) tmp[[f]] <- jsonlite::fromJSON(files[f])
+  res.df <- as.data.frame(do.call(rbind, tmp))
+  # list-columns to comma-separated strings
+  res.df[] <- lapply(res.df, function(col) {
+    if (is.list(col)) sapply(col, toString) else col
+  })
+  
+  res$setHeader(
+    "Content-Disposition",
+    paste0('attachment; filename="session_data_', format(Sys.time(), "%Y-%m-%d_%H-%M"), '.csv"')
+  )
+  return(res.df)
+}
+
+#* UPLOAD FILES FROM GITHUB PAGE
+#* @post /upload
+#* @parser multi
+function(req, res) {
+  
+  api_key <- req$HTTP_X_API_KEY
+  if (is.null(api_key) || api_key != KEY) {
+    res$status <- 401
+    return(list(error = "Invalid or missing API key"))
+  }
+  upload_dir <- "uploads"
+  if (!dir.exists(upload_dir)) 
+    dir.create(upload_dir, showWarnings = FALSE)
+  
+  for(i in 1:length(req$body)){
+    dest <- file.path("uploads", req$body[[i]]$filename)
+    writeBin(req$body[[i]]$value, dest)  
+  }
+  
+  list(
+    status = "ok",
+    saved_as = basename(dest)
+  )
+}
