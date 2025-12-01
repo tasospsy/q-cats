6b518cc88ba6c43d9e5470193eeea178956c0aee
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2025-12-01 11:59:07 +0200

    bug

--- catapi.R
+++ catapi.R
@@ -122,7 +122,7 @@ function(req, res) {
     #stop_crit = user$stop_crit,
     next_q = df$Item[next_item_num],
     choices = as.character(df[next_item_num, 3:6]),
-    item_num = next_item_num,
+    item_num = next_item_num
     #itembank = user$itembank
   )
 }
